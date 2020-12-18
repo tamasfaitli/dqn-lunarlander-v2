9d0f98e446c4bda56d8013d8664d1265dcb87acb
Tamas Faitli <faitlit@gmail.com>
2020-12-18 21:05:42 +0100

    initial commit

---
+++ problem_1.py
@@ -0,0 +1,16 @@
+#################################################
+#                                               #
+# EL2805 Reinforcement Learning                 #
+# Computer Lab 2                                #
+# Problem 1                                     #
+#                                               #
+# Author: Tamas Faitli (19960205-T410)          #
+#                                               #
+#################################################
+
+DEF_ENV = 'LunarLander-v2'
+DEF_OUTP_FILE = 'neural-network-1'
+
+
+if __name__ == '__main__':
+    pass
